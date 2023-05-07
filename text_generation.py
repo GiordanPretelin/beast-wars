32cfe4a2b0b6e3513c2610c1c4c84be41d8896f3
giordan <giordanpretlein@gmail.com>
2023-05-07 19:05:09 +0200

    Adding all locally created files

---
+++ text_generation.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from transformers import pipeline
+
+text = """Dear Amazon, last week I ordered an Optimus Prime action figure
+from your online store in Germany. Unfortunately, when I opened the package,
+I discovered to my horror that I had been sent an action figure of Megatron
+instead! As a lifelong enemy of the Decepticons, I hope you can understand my
+dilemma. To resolve the issue, I demand an exchange of Megatron for the
+Optimus Prime figure I ordered. Enclosed are copies of my records concerning
+this purchase. I expect to hear from you soon. Sincerely, Bumblebee."""
+
+generator = pipeline("text-generation")
+response = "Dear Bumblebee, I am sorry to hear that your order was mixed up."
+prompt = text + "\n\nCustomer service response:\n" + response
+outputs = generator(prompt, max_length=200)
+print(outputs[0]['generated_text'])
